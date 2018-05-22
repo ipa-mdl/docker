1144a4688a8ad2475d2003b0cc1532794fec9bfc
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2018-05-22 02:24:43 +0200

    handle trailing slash

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ for a in "$@"; do
   for f in $(find "$a" -name Dockerfile); do
     d="${f%/Dockerfile}"
     tag=$(basename "$d")
-    if [ "$tag" = "$d" ]; then
-        tag=$DEFAULT_TAG
+    if [ "$tag" = "$d"* ]; then
+      tag=$DEFAULT_TAG
     fi
     repo=${d%/$tag}
     t="rosindustrial/${repo//\//-}:$tag" # replace other slashed with dashes
